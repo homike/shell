64700b7cf42e7cf9a6ed765185ea247146b270cc
moyu <403231700@qq.com>
2020-10-12 17:51:26 +0800

    Update mysql57.sh

--- install/mysql57.sh
+++ install/mysql57.sh
@@ -6,20 +6,20 @@ if  [ ! -n "$1" ] ;then
 fi
 password=$1
 
-### download mysql yum package
-#wget http://repo.mysql.com/mysql57-community-release-el7-10.noarch.rpm
-#
-## install mysqll source
-#rpm -Uvh mysql57-community-release-el7-10.noarch.rpm
-#
-## install
-#yum install -y mysql-community-server
-#
-## start mysql
-#systemctl start mysqld.service
-#
-## check mysql status
-#systemctl status mysqld.service
+## download mysql yum package
+wget http://repo.mysql.com/mysql57-community-release-el7-10.noarch.rpm
+
+# install mysqll source
+rpm -Uvh mysql57-community-release-el7-10.noarch.rpm
+
+# install
+yum install -y mysql-community-server
+
+# start mysql
+systemctl start mysqld.service
+
+# check mysql status
+systemctl status mysqld.service
 
 # get temporary password
 grep 'temporary password' /var/log/mysqld.log > temp_password.txt
@@ -28,7 +28,6 @@ rm -f temp_password.txt
 echo "[ password ]"  $tempPassword  $password
 
 # set password and remote access
-echo mysql --connect-expired-password -uroot -p"$tempPassword"
 mysql --connect-expired-password -uroot -p"$tempPassword" -e"set global validate_password_policy=0;set global validate_password_length=1;ALTER USER 'root'@'localhost' IDENTIFIED BY '$password';"
 mysql --connect-expired-password -uroot -p"$password" -e"GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$password' WITH GRANT OPTION;FLUSH PRIVILEGES;"
 
